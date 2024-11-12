13e7ac27a07591767d4e508006e0ab3a39786fe5
nyxnick <bascomza04@gmail.com>
2024-11-12 23:29:46 +0700

    edit font

--- app/static/assets/js/reward_success.js
+++ app/static/assets/js/reward_success.js
@@ -2,4 +2,7 @@ Swal.fire({
     title: "สแกนรับรางวัลสำเร็จ",
     text: "",
     icon: "success",
-  });
+    customClass: {
+        popup: 'custom-margin'  // ใช้คลาสนี้เพื่อเพิ่มระยะห่าง
+    }
+});
